ca73f68e26516f4c85ca778687a9ed14eb424a4b
Kevin Dillane <dillane.k@gmail.com>
2023-06-28 22:13:09 -0700

    Update

--- custom_components/heater_cooler/recorder.py
+++
@@ -1,9 +0,0 @@
-"""Integration platform for recorder."""
-from __future__ import annotations
-
-from homeassistant.core import HomeAssistant, callback
-
-@callback
-def exclude_attributes(hass: HomeAssistant) -> set[str]:
-    """Exclude static attributes from being recorded in the database."""
-    return { }
